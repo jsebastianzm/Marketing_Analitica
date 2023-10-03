2e7bec1ef96279c9938d58929c02b193baedca22
jsebastianzm <2000jszm@protonmail.com>
2023-10-03 11:58:29 -0500

    Se termina el 4 modelo de recomendación, queda pendiente el despliegue

--- c_modelos.py
+++ c_modelos.py
@@ -8,6 +8,9 @@ import joblib
 from mlxtend.preprocessing import TransactionEncoder
 
 from surprise import Reader, Dataset
+from surprise import KNNBasic, KNNWithMeans, KNNWithZScore, KNNBaseline
+from surprise.model_selection import cross_validate, GridSearchCV
+
 #### conectar_base_de_Datos
 
 conn=sql.connect('Datos//db_movies_2')
@@ -209,99 +212,93 @@ print(interact(recomendar))
 #####4 Sistema de recomendación filtro colaborativo #####
 ############################################################################
 
-# ### datos originales en pandas
-# ## knn solo sirve para calificaciones explicitas
-# ratings=pd.read_sql('select * from ratings_final where rating>0', conn)
 
+pd.read_sql('select * from ratings_final where rating>0', conn)
 
-# ####los datos deben ser leidos en un formato espacial para surprise
-# reader = Reader(rating_scale=(0, 10)) ### la escala de la calificación
-# ###las columnas deben estar en orden estándar: user item rating
-# data   = Dataset.load_from_df(ratings[['user_id','isbn','book_rating']], reader)
+### datos originales en pandas
+## knn solo sirve para calificaciones explicitas
+ratings=pd.read_sql('select * from ratings_final where rating>0', conn)
 
+####los datos deben ser leidos en un formato espacial para surprise
+reader = Reader(rating_scale=(0, 5)) ### la escala de la calificación
+###las columnas deben estar en orden estándar: user item rating
+data   = Dataset.load_from_df(ratings[['userId','movieId','rating']], reader)
 
-# #####Existen varios modelos 
-# models=[KNNBasic(),KNNWithMeans(),KNNWithZScore(),KNNBaseline()] 
-# results = {}
+#####Existen varios modelos
+models=[KNNBasic(),KNNWithMeans(),KNNWithZScore(),KNNBaseline()]
+results = {}
 
-# ###knnBasiscs: calcula el rating ponderando por distancia con usuario/Items
-# ###KnnWith means: en la ponderación se resta la media del rating, y al final se suma la media general
-# ####KnnwithZscores: estandariza el rating restando media y dividiendo por desviación 
-# ####Knnbaseline: calculan el desvío de cada calificación con respecto al promedio y con base en esos calculan la ponderación
+#### función para probar varios modelos ##########
+model=models[1]
+for model in models:
 
+    CV_scores = cross_validate(model, data, measures=["MAE","RMSE"], cv=5, n_jobs=-1)
 
-# #### función para probar varios modelos ##########
-# model=models[1]
-# for model in models:
- 
-#     CV_scores = cross_validate(model, data, measures=["MAE","RMSE"], cv=5, n_jobs=-1)  
-    
-#     result = pd.DataFrame.from_dict(CV_scores).mean(axis=0).\
-#              rename({'test_mae':'MAE', 'test_rmse': 'RMSE'})
-#     results[str(model).split("algorithms.")[1].split("object ")[0]] = result
+    result = pd.DataFrame.from_dict(CV_scores).mean(axis=0).\
+             rename({'test_mae':'MAE', 'test_rmse': 'RMSE'})
+    results[str(model).split("algorithms.")[1].split("object ")[0]] = result
 
+performance_df = pd.DataFrame.from_dict(results).T
+performance_df.sort_values(by='RMSE')
 
-# performance_df = pd.DataFrame.from_dict(results).T
-# performance_df.sort_values(by='RMSE')
+param_grid = {
+    'sim_options': {
+        'name': ['msd', 'cosine'],
+        'min_support': [5],
+        'user_based': [False, True]
+    },
+    'bsl_options': {
+        'method': ['als', 'sgd'],  # Opciones para KNNBaseline
+    }
+}
 
-# ###################se escoge el mejor knn withmeans#########################
-# param_grid = { 'sim_options' : {'name': ['msd','cosine'], \
-#                                 'min_support': [5], \
-#                                 'user_based': [False, True]}
-#              }
+### se afina si es basado en usuario o basado en ítem
 
-# ### se afina si es basado en usuario o basado en ítem
+gridsearchKNNBaseline = GridSearchCV(KNNBaseline, param_grid, measures=['rmse'], \
+                                      cv=2, n_jobs=2)
 
-# gridsearchKNNWithMeans = GridSearchCV(KNNWithMeans, param_grid, measures=['rmse'], \
-#                                       cv=2, n_jobs=2)
-                                    
-# gridsearchKNNWithMeans.fit(data)
+gridsearchKNNBaseline.fit(data)
 
+gridsearchKNNBaseline.best_params["rmse"]
 
-# gridsearchKNNWithMeans.best_params["rmse"]
-# gridsearchKNNWithMeans.best_score["rmse"]
-# gs_model=gridsearchKNNWithMeans.best_estimator['rmse'] ### mejor estimador de gridsearch
+gridsearchKNNBaseline.best_score["rmse"]
 
+gs_model=gridsearchKNNBaseline.best_estimator['rmse'] ### mejor estimador de gridsearch
+gs_model
 
-# ################# Entrenar con todos los datos y Realizar predicciones con el modelo afinado
+################# Entrenar con todos los datos y Realizar predicciones con el modelo afinado
 
-# trainset = data.build_full_trainset() ### esta función convierte todos los datos en entrnamiento, las funciones anteriores dividen  en entrenamiento y evaluación
-# model=gs_model.fit(trainset) ## se reentrena sobre todos los datos posibles (sin dividir)
+trainset = data.build_full_trainset() ### esta función convierte todos los datos en entrnamiento, las funciones anteriores dividen  en entrenamiento y evaluación
+model=gs_model.fit(trainset) ## se reentrena sobre todos los datos posibles (sin dividir)
 
-# predset = trainset.build_anti_testset() ### crea una tabla con todos los usuarios y los libros que no han leido
-# #### en la columna de rating pone el promedio de todos los rating, en caso de que no pueda calcularlo para un item-usuario
-# len(predset)
+predset = trainset.build_anti_testset() ### crea una tabla con todos los usuarios y los libros que no han leido
+#### en la columna de rating pone el promedio de todos los rating, en caso de que no pueda calcularlo para un item-usuario
+len(predset)
 
-# predictions = gs_model.test(predset) ### función muy pesada, hace las predicciones de rating para todos los libros que no hay leido un usuario
-# ### la funcion test recibe un test set constriuido con build_test method, o el que genera crosvalidate
+predictions = gs_model.test(predset) ### función muy pesada, hace las predicciones de rating para todos las peliculas que no hay leido un usuario
+### la funcion test recibe un test set constriuido con build_test method, o el que genera crosvalidate
 
-# predictions_df = pd.DataFrame(predictions) ### esta tabla se puede llevar a una base donde estarán todas las predicciones
-# predictions_df.shape
-# predictions_df.head()
-# predictions_df['r_ui'].unique() ### promedio de ratings
-# predictions_df.sort_values(by='est',ascending=False)
+predictions_df = pd.DataFrame(predictions) ### esta tabla se puede llevar a una base donde estarán todas las predicciones
+predictions_df.shape
 
-# ####### la predicción se puede hacer para un libro puntual
-# model.predict(uid=269397, iid='0446353205',r_ui='') ### uid debía estar en número e isb en comillas
+predictions_df.head()
 
+predictions_df['r_ui'].unique() ### promedio de ratings
+predictions_df.sort_values(by='est',ascending=False)
 
-# ##### funcion para recomendar los 10 libros con mejores predicciones y llevar base de datos para consultar resto de información
-# def recomendaciones(user_id,n_recomend=10):
-    
-#     predictions_userID = predictions_df[predictions_df['uid'] == user_id].\
-#                     sort_values(by="est", ascending = False).head(n_recomend)
+##### funcion para recomendar los 10 libros con mejores predicciones y llevar base de datos para consultar resto de información
+def recomendaciones(user_id,n_recomend=10):
 
-#     recomendados = predictions_userID[['iid','est']]
-#     recomendados.to_sql('reco',conn,if_exists="replace")
-    
-#     recomendados=pd.read_sql('''select a.*, b.book_title 
-#                              from reco a left join books_final b
-#                              on a.iid=b.isbn ''', conn)
+    predictions_userID = predictions_df[predictions_df['uid'] == user_id].\
+                    sort_values(by="est", ascending = False).head(n_recomend)
 
-#     return(recomendados)
+    recomendados = predictions_userID[['iid','est']]
+    recomendados.to_sql('reco',conn,if_exists="replace")
 
+    recomendados=pd.read_sql('''select a.*, b.title
+                             from reco a left join movies_final b
+                             on a.iid=b.movieId ''', conn)
 
- 
-# recomendaciones(user_id=179733,n_recomend=10)
+    return(recomendados)
 
-
+recomendaciones(user_id=429,n_recomend=10)
